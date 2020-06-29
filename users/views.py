8fc9efea9495b59008e52effc238dd97f42e8d89
Amlana-P <amlana.pattnayak@gmail.com>
2020-06-29 12:20:29 +0530

    Django-Project-Elamigo

---
+++ users/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import connection
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created. Now you can login!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+def fetchUserData(request):
+	context = {
+	'userData': User.objects.all()
+	}
+	return render(request, 'users/profile.html', context)
